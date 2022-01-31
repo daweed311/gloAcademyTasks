a7af21248f7355e0e5458a63e0483a8634f547cf
David Melnik <davidmelnik483@gmail.com>
2022-01-31 15:18:18 +0200

    3 урок усложненные задачи

---
+++ lesson03_complicated/script.js
@@ -0,0 +1,70 @@
+const lang = prompt("Выберите язык", "ru/en").toLocaleLowerCase().trim();
+
+// Через конструкцию if:
+
+if (lang == "ru") {
+  console.log(
+    "Понедельник, Вторник, Среда, Четверг, Пятница, Суббота, Восресенье"
+  );
+} else if (lang == "en") {
+  console.log("Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday");
+} else {
+  alert("Выбирайте только между en и ru");
+}
+
+// Через switch-case:
+
+// switch (lang) {
+//   case "ru":
+//     console.log(
+//       "Понедельник, Вторник, Среда, Четверг, Пятница, Суббота, Восресенье"
+//     );
+//     break;
+//   case "en":
+//     console.log(
+//       "Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday"
+//     );
+//     break;
+//   default:
+//     alert("Выбирайте только между en и ru");
+//     break;
+// }
+
+// Через многомерный массив без ифов и switch.
+
+// let arr = [
+//   [
+//     "Понедельник",
+//     "Вторник",
+//     "Среда",
+//     "Четверг",
+//     "Пятница",
+//     "Суббота",
+//     "Восресенье",
+//   ],
+//   [
+//     "Monday",
+//     "Tuesday",
+//     "Wednesday",
+//     "Thursday",
+//     "Friday",
+//     "Saturday",
+//     "Sunday",
+//   ],
+// ];
+
+// lang == "ru"
+//   ? console.log(arr[0])
+//   : lang == "en"
+//   ? console.log(arr[1])
+//   : console.log("Выбирайте только между en и ru");
+
+// задача c namePerson
+
+const namePerson = prompt("Введите имя");
+
+namePerson == "Артем"
+  ? console.log("директор")
+  : namePerson == "Александр"
+  ? console.log("преподаватель")
+  : console.log("студент");
