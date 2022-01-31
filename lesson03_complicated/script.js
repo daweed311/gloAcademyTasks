fcaccfc1103af156c292a3b74fae89e7072acafa
David Melnik <davidmelnik483@gmail.com>
2022-01-31 15:22:37 +0200

    3 урок усложненное задание

--- lesson03_complicated/script.js
+++ lesson03_complicated/script.js
@@ -2,15 +2,15 @@ const lang = prompt("Выберите язык", "ru/en").toLocaleLowerCase().tr
 
 // Через конструкцию if:
 
-if (lang == "ru") {
-  console.log(
-    "Понедельник, Вторник, Среда, Четверг, Пятница, Суббота, Восресенье"
-  );
-} else if (lang == "en") {
-  console.log("Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday");
-} else {
-  alert("Выбирайте только между en и ru");
-}
+// if (lang == "ru") {
+//   console.log(
+//     "Понедельник, Вторник, Среда, Четверг, Пятница, Суббота, Восресенье"
+//   );
+// } else if (lang == "en") {
+//   console.log("Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday");
+// } else {
+//   alert("Выбирайте только между en и ru");
+// }
 
 // Через switch-case:
 
@@ -32,39 +32,39 @@ if (lang == "ru") {
 
 // Через многомерный массив без ифов и switch.
 
-// let arr = [
-//   [
-//     "Понедельник",
-//     "Вторник",
-//     "Среда",
-//     "Четверг",
-//     "Пятница",
-//     "Суббота",
-//     "Восресенье",
-//   ],
-//   [
-//     "Monday",
-//     "Tuesday",
-//     "Wednesday",
-//     "Thursday",
-//     "Friday",
-//     "Saturday",
-//     "Sunday",
-//   ],
-// ];
+let arr = [
+  [
+    "Понедельник",
+    "Вторник",
+    "Среда",
+    "Четверг",
+    "Пятница",
+    "Суббота",
+    "Восресенье",
+  ],
+  [
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+    "Sunday",
+  ],
+];
 
-// lang == "ru"
-//   ? console.log(arr[0])
-//   : lang == "en"
-//   ? console.log(arr[1])
-//   : console.log("Выбирайте только между en и ru");
+lang == "ru"
+  ? console.log(arr[0].toString())
+  : lang == "en"
+  ? console.log(arr[1].toString())
+  : console.log("Выбирайте только между en и ru");
 
 // задача c namePerson
 
-const namePerson = prompt("Введите имя");
+// const namePerson = prompt("Введите имя");
 
-namePerson == "Артем"
-  ? console.log("директор")
-  : namePerson == "Александр"
-  ? console.log("преподаватель")
-  : console.log("студент");
+// namePerson == "Артем"
+//   ? console.log("директор")
+//   : namePerson == "Александр"
+//   ? console.log("преподаватель")
+//   : console.log("студент");
