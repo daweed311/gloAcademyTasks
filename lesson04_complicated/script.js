f095e4a2174936cfbd15468883a952422fcf48c5
David Melnik <davidmelnik483@gmail.com>
2022-02-03 19:45:14 +0200

    решил

--- lesson04_complicated/script.js
+++ lesson04_complicated/script.js
@@ -1,1 +1,22 @@
-// пока в процессе
+"use strict";
+
+const myFunc = function (arg) {
+  let someString;
+  if (typeof arg === "string") {
+    const limitLength = 30;
+    const str = arg.trim();
+    if (str.length > limitLength) {
+      someString = str.substr(0, limitLength) + "...";
+    } else {
+      someString = str;
+    }
+  }
+  return someString;
+};
+
+console.log(myFunc("   bla bla bla bla  "));
+console.log(
+  myFunc(
+    "   bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla "
+  )
+);
